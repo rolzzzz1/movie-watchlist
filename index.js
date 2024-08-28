105893b07eb6d8ac21ac9432acc3ae9789e88737
Aishwarya Sharma <roli.mradul@gmail.com>
2024-08-28 18:29:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-fetch(
-  "http://www.omdbapi.com/?apikey=http://www.omdbapi.com/?t=blade+runner&t=blade+runner"
-)
+fetch("http://www.omdbapi.com/?apikey=cddaec6f&t=blade+runner")
   .then((res) => res.json())
   .then((data) => console.log(data));
