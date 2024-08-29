7d0b056fe9f798f81477256ae211fc297ae5fb49
Aishwarya Sharma <roli.mradul@gmail.com>
2024-08-29 18:03:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,34 +22,36 @@ function handleSearch() {
 }
 
 function populateMovie(data) {
-  movieList.innerHTML += `
-    <div class="movieData">
-      <img
-        id="moviePoster"
-        src=${data.Poster}
-        alt="Movie poster"
-        class="moviePoster"
-      />
-      <div class="movieDataText">
-        <div class="movieData-top">
-          <h2 class="inter-medium">${data.Title}</h2>
-          <p class="inter-regular">
-            <img src="/img/star.png" /> ${data.Ratings[0].Value.split("/")[0]}
-          </p>
-        </div>
-        <div class="movieData-middle">
-          <p class="inter-regular">${data.Runtime}</p>
-          <p class="inter-regular">${data.Genre}</p>
-          <button class="plusBtn inter-regular">
-            <img src="/img/plusIcon.png" /> Watchlist
-          </button>
-        </div>
-        <p class="inter-regular">
-          ${data.Plot}
-        </p>
-      </div>
-    </div>;
-  `;
+  console.log(data);
+
+  // movieList.innerHTML += `
+  //   <div class="movieData">
+  //     <img
+  //       id="moviePoster"
+  //       src=${data.Poster}
+  //       alt="Movie poster"
+  //       class="moviePoster"
+  //     />
+  //     <div class="movieDataText">
+  //       <div class="movieData-top">
+  //         <h2 class="inter-medium">${data.Title}</h2>
+  //         <p class="inter-regular">
+  //           <img src="/img/star.png" /> ${data.Ratings[0].Value.split("/")[0]}
+  //         </p>
+  //       </div>
+  //       <div class="movieData-middle">
+  //         <p class="inter-regular">${data.Runtime}</p>
+  //         <p class="inter-regular">${data.Genre}</p>
+  //         <button class="plusBtn inter-regular">
+  //           <img src="/img/plusIcon.png" /> Watchlist
+  //         </button>
+  //       </div>
+  //       <p class="inter-regular">
+  //         ${data.Plot}
+  //       </p>
+  //     </div>
+  //   </div>;
+  // `;
 }
 
 /* <div class="movieData">
