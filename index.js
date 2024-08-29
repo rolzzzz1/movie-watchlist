f8870bfa38e03d1d24a64d426b4f0c9748c9f268
Aishwarya Sharma <roli.mradul@gmail.com>
2024-08-29 06:19:50 +0530

    next steps

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-fetch("https://www.omdbapi.com/?apikey=cddaec6f&t=blade+runner")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const searchBtn = document.getElementById("searchBtn");
+const movieInput = document.getElementById("movieInput");
+
+searchBtn.addEventListener("click", handleSearch);
+
+function handleSearch() {
+  console.log(movieInput.value);
+
+  fetch("https://www.omdbapi.com/?apikey=cddaec6f&t=blade+runner")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
