9bff438432125b232180139d7c8a5cbcca3fabe0
Aishwarya Sharma <roli.mradul@gmail.com>
2024-08-29 17:42:48 +0530

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const searchBtn = document.getElementById("searchBtn");
 const movieInput = document.getElementById("movieInput");
 const searchForm = document.getElementById("searchForm");
+const movieList = document.getElementById("movieList");
 
 // searchBtn.addEventListener("click", (event) => {
 //   event.preventDefault();
@@ -17,5 +18,65 @@ function handleSearch() {
 
   fetch(`https://www.omdbapi.com/?apikey=cddaec6f&t=${movieInput.value}`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => populateMovie(data));
 }
+
+function populateMovie(data) {
+  movieList.innerHTML += `
+    <div class="movieData">
+      <img
+        id="moviePoster"
+        src=${data.Poster}
+        alt="Movie poster"
+        class="moviePoster"
+      />
+      <div class="movieDataText">
+        <div class="movieData-top">
+          <h2 class="inter-medium">Blade Runner</h2>
+          <p class="inter-regular">
+            <img src="/img/star.png" /> 8.1
+          </p>
+        </div>
+        <div class="movieData-middle">
+          <p class="inter-regular">117min</p>
+          <p class="inter-regular">Action, Drama, Sci-fi</p>
+          <button class="plusBtn inter-regular">
+            <img src="/img/plusIcon.png" /> Watchlist
+          </button>
+        </div>
+        <p class="inter-regular">
+          A blade runner must pursue and terminate four replicants who stole a ship
+          in space, and have returned to Earth to find their creator.
+        </p>
+      </div>
+    </div>;
+  `;
+}
+
+/* <div class="movieData">
+  <img
+    id="moviePoster"
+    src="https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+    alt="Movie poster"
+    class="moviePoster"
+  />
+  <div class="movieDataText">
+    <div class="movieData-top">
+      <h2 class="inter-medium">Blade Runner</h2>
+      <p class="inter-regular">
+        <img src="/img/star.png" /> 8.1
+      </p>
+    </div>
+    <div class="movieData-middle">
+      <p class="inter-regular">117min</p>
+      <p class="inter-regular">Action, Drama, Sci-fi</p>
+      <button class="plusBtn inter-regular">
+        <img src="/img/plusIcon.png" /> Watchlist
+      </button>
+    </div>
+    <p class="inter-regular">
+      A blade runner must pursue and terminate four replicants who stole a ship
+      in space, and have returned to Earth to find their creator.
+    </p>
+  </div>
+</div>; */
