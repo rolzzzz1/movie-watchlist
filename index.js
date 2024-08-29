d70d011464cfec80be84fd3e2ae1b3cc0b188e35
Aishwarya Sharma <roli.mradul@gmail.com>
2024-08-29 14:09:04 +0530

    fix

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ searchForm.addEventListener("submit", (event) => {
 });
 
 function handleSearch() {
-  console.log(movieInput.value);
+  // fetch("https://www.omdbapi.com/?apikey=cddaec6f&t=blade+runner")
 
   fetch(`https://www.omdbapi.com/?apikey=cddaec6f&t=${movieInput.value}`)
-    // fetch("https://www.omdbapi.com/?apikey=cddaec6f&t=blade+runner")
     .then((res) => res.json())
     .then((data) => console.log(data));
 }
