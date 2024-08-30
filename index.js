aaed8533591cb644df268f607ab83fe0d86f7ca4
Aishwarya Sharma <roli.mradul@gmail.com>
2024-08-30 18:28:48 +0530

    fix

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// const searchBtn = document.getElementById("searchBtn");
 const movieInput = document.getElementById("movieInput");
 const searchForm = document.getElementById("searchForm");
 const movieList = document.getElementById("movieList");
-
-// searchBtn.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   handleSearch();
-// });
+const startExplore = document.getElementById("startExplore");
 
 searchForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -14,11 +9,15 @@ searchForm.addEventListener("submit", (event) => {
 });
 
 function handleSearch() {
-  // fetch("https://www.omdbapi.com/?apikey=cddaec6f&t=blade+runner")
-
-  fetch(`https://www.omdbapi.com/?apikey=cddaec6f&s=${movieInput.value}`)
-    .then((res) => res.json())
-    .then((data) => populateMovie(data.Search));
+  fetch(`https://www.omdbapi.com/?apikey=cddaec6f&s=${movieInput.value1}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Something went wrong...");
+      }
+      return res.json();
+    })
+    .then((data) => populateMovie(data.Search))
+    .catch((err) => console.log(err));
 }
 
 function populateMovie(mList) {
@@ -64,40 +63,5 @@ function populateMovie(mList) {
           </div>
         `;
       });
-
-    let plusButtons = document.querySelectorAll("plusBtn");
-    console.log(plusButtons);
   }
 }
-
-// plusBtn.addEventListener("click", function handlePlusClick() {
-//   console.log(plusBtn.getAttribute("data-id"));
-// });
-
-/* <div class="movieData">
-  <img
-    id="moviePoster"
-    src="https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-    alt="Movie poster"
-    class="moviePoster"
-  />
-  <div class="movieDataText">
-    <div class="movieData-top">
-      <h2 class="inter-medium">Blade Runner</h2>
-      <p class="inter-regular">
-        <img src="/img/star.png" /> 8.1
-      </p>
-    </div>
-    <div class="movieData-middle">
-      <p class="inter-regular">117min</p>
-      <p class="inter-regular">Action, Drama, Sci-fi</p>
-      <button class="plusBtn inter-regular">
-        <img src="/img/plusIcon.png" /> Watchlist
-      </button>
-    </div>
-    <p class="inter-regular">
-      A blade runner must pursue and terminate four replicants who stole a ship
-      in space, and have returned to Earth to find their creator.
-    </p>
-  </div>
-</div>; */
