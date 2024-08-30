f356ecd63fd28218e1fa319c1a7e5165f87d8c93
Aishwarya Sharma <roli.mradul@gmail.com>
2024-08-30 09:04:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,36 +30,39 @@ function populateMovie(mList) {
 
     fetch(`https://www.omdbapi.com/?apikey=cddaec6f&i=${currentMovie.imdbID}`)
       .then((res) => res.json())
-      .then((data) => console.log(data));
-
-    movieList.innerHTML += `
-      <div class="movieData">
-        <img
-          id="moviePoster"
-          src=${data.Poster}
-          alt="Movie poster"
-          class="moviePoster"
-        />
-        <div class="movieDataText">
-          <div class="movieData-top">
-            <h2 class="inter-medium">${data.Title}</h2>
-            <p class="inter-regular">
-              <img src="/img/star.png" /> ${data.Ratings[0].Value.split("/")[0]}
-            </p>
-          </div>
-          <div class="movieData-middle">
-            <p class="inter-regular">${data.Runtime}</p>
-            <p class="inter-regular">${data.Genre}</p>
-            <button class="plusBtn inter-regular">
-              <img src="/img/plusIcon.png" /> Watchlist
-            </button>
-          </div>
-          <p class="inter-regular">
-            ${data.Plot}
-          </p>
-        </div>
-      </div>;
-    `;
+      .then((data) => {
+        console.log(data);
+        movieList.innerHTML += `
+          <div class="movieData">
+            <img
+              id="moviePoster"
+              src=${data.Poster}
+              alt="Movie poster"
+              class="moviePoster"
+            />
+            <div class="movieDataText">
+              <div class="movieData-top">
+                <h2 class="inter-medium">${data.Title}</h2>
+                <p class="inter-regular">
+                  <img src="/img/star.png" /> ${
+                    data.Ratings[0].Value.split("/")[0]
+                  }
+                </p>
+              </div>
+              <div class="movieData-middle">
+                <p class="inter-regular">${data.Runtime}</p>
+                <p class="inter-regular">${data.Genre}</p>
+                <button class="plusBtn inter-regular">
+                  <img src="/img/plusIcon.png" /> Watchlist
+                </button>
+              </div>
+              <p class="inter-regular">
+                ${data.Plot}
+              </p>
+            </div>
+          </div>;
+        `;
+      });
   }
 }
 
