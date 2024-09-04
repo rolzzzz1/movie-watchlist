faca5620fc81c4177a738176275bb6600cab2cc1
Aishwarya Sharma <roli.mradul@gmail.com>
2024-09-04 19:33:40 +0530

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -3,6 +3,9 @@ const watchList = document.getElementById("watchList");
 function renderMyWatchlist() {
   myWatchlist = JSON.parse(localStorage.getItem("watchlist"));
   console.log(myWatchlist);
+  for (let index in myWatchlist) {
+    console.log(myWatchlist[index]);
+  }
 }
 
 renderMyWatchlist();
