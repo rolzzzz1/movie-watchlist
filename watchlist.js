f0f488c3e96d2043f6c62ea01a9c13db2d65e421
Aishwarya Sharma <roli.mradul@gmail.com>
2024-09-04 19:31:15 +0530

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -2,11 +2,9 @@ const watchList = document.getElementById("watchList");
 
 function renderMyWatchlist() {
   myWatchlist = localStorage.getItem("watchlist");
-  console.log(myWatchlist);
-
-  for (let id in myWatchlist) {
-    console.log(myWatchlist[id]);
-  }
+  console.log(myWatchlist[0]);
+  console.log(myWatchlist[1]);
+  console.log(myWatchlist[2]);
 }
 
 renderMyWatchlist();
