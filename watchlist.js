6fc94498dee423a33121220a749ebb93f87742a3
Aishwarya Sharma <roli.mradul@gmail.com>
2024-09-07 21:32:56 +0530

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -5,56 +5,54 @@ function renderMyWatchlist() {
   myWatchlist = JSON.parse(localStorage.getItem("watchlist"));
   console.log(myWatchlist);
 
-  for (let item in myWatchlist) {
-    console.log(item);
-    console.log(myWatchlist[item]);
-    console.log(myWatchlist[item].id);
-  }
+  if (Object.keys(myWatchlist).length === 0) {
+    console.log("Object is empty");
+  } else {
+    console.log("Not empty");
+    for (let item in myWatchlist) {
+      console.log(myWatchlist[item].id);
 
-  // let movie = myWatchlist.find((movie) => movie.id === "tt1856101");
-  // console.log(movie);
+      fetch(
+        `https://www.omdbapi.com/?apikey=cddaec6f&i=${myWatchlist[item].id}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
 
-  for (let item in myWatchlist) {
-    console.log(myWatchlist[item].id);
-
-    fetch(`https://www.omdbapi.com/?apikey=cddaec6f&i=${myWatchlist[item].id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-
-        watchList.innerHTML += `
-          <div class="movieData">
-            <img
-              id="moviePoster"
-              src=${data.Poster}
-              alt="Movie poster"
-              class="moviePoster"
-            />
-            <div class="movieDataText">
-              <div class="movieData-top">
-                <h2 class="inter-medium">${data.Title}</h2>
+          watchList.innerHTML += `
+            <div class="movieData">
+              <img
+                id="moviePoster"
+                src=${data.Poster}
+                alt="Movie poster"
+                class="moviePoster"
+              />
+              <div class="movieDataText">
+                <div class="movieData-top">
+                  <h2 class="inter-medium">${data.Title}</h2>
+                  <p class="inter-regular">
+                    <img src="/img/star.png" /> ${
+                      data.Ratings[0].Value.split("/")[0]
+                    }
+                  </p>
+                </div>
+                <div class="movieData-middle">
+                  <p class="inter-regular">${data.Runtime}</p>
+                  <p class="inter-regular">${data.Genre}</p>
+                  <button class="plusBtn inter-regular" data-id=${data.imdbID} >
+                    <img src="/img/removeIcon.png" data-id=${
+                      data.imdbID
+                    }  /> Remove
+                  </button>
+                </div>
                 <p class="inter-regular">
-                  <img src="/img/star.png" /> ${
-                    data.Ratings[0].Value.split("/")[0]
-                  }
+                  ${data.Plot}
                 </p>
               </div>
-              <div class="movieData-middle">
-                <p class="inter-regular">${data.Runtime}</p>
-                <p class="inter-regular">${data.Genre}</p>
-                <button class="plusBtn inter-regular" data-id=${data.imdbID} >
-                  <img src="/img/removeIcon.png" data-id=${
-                    data.imdbID
-                  }  /> Remove
-                </button>
-              </div>
-              <p class="inter-regular">
-                ${data.Plot}
-              </p>
             </div>
-          </div>
-        `;
-      });
+          `;
+        });
+    }
   }
 }
 
