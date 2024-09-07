a9468c4acff3e008f4c87079b01c998db9575d8f
Aishwarya Sharma <roli.mradul@gmail.com>
2024-09-07 21:15:48 +0530

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -71,6 +71,7 @@ watchList.addEventListener("click", (event) => {
     myWatchlist = myWatchlist.filter((item) => item.id !== movieId);
     console.log(myWatchlist);
 
+    localStorage.setItem("watchlist", JSON.stringify(myWatchlist));
     renderMyWatchlist();
   }
 });
