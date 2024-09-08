0c3f9c34fcea9601a1f558171a5dc641760ae20e
Aishwarya Sharma <roli.mradul@gmail.com>
2024-09-08 18:32:22 +0530

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -4,10 +4,9 @@ let myWatchlist;
 function renderMyWatchlist() {
   myWatchlist = JSON.parse(localStorage.getItem("watchlist"));
   console.log(myWatchlist);
-
+  watchList.innerHTML = "";
   if (Object.keys(myWatchlist).length === 0) {
     console.log("Object is empty");
-    watchList.innerHTML = "";
   } else {
     console.log("Not empty");
     for (let item in myWatchlist) {
